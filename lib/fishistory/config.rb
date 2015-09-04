3adae44fcfa8e4e9ae42507a8d8282f3e4ed39ab
Nate Collings <nate@natecollings.com>
2015-09-04 15:01:04 -0600

    database config setup

--- lib/fishistory/config.rb
+++ lib/fishistory/config.rb
@@ -1,9 +1,13 @@
 module Fishistory
   class Config
-    attr_accessor :fish_root_path
+    attr_accessor :fish_root_path, :adapter, :database, :username, :password
 
     def initialize
       @fish_root_path = "~/.config/fish"
+      @adapter = 'sqlite3'
+      @database = 'fishistory'
+      @username = 'root'
+      @password = 'password'
     end 
   end
 end
--- spec/fishistory_spec.rb
+++ spec/fishistory_spec.rb
@@ -2,14 +2,36 @@ require "spec_helper"
 
 describe Fishistory do
   describe "#configure" do
+    let(:subject) { Fishistory.config}
+
     it "defaults fish_root_path to ~/.config/fish" do
-      expect(Fishistory.config.fish_root_path).to eq('~/.config/fish')
+      expect(subject.fish_root_path).to eq('~/.config/fish')
     end
 
     it "allows you to set fish_root_path" do
       Fishistory.configure { |c| c.fish_root_path = '~/fish/are/tasty' }
+      expect(subject.fish_root_path).to eq('~/fish/are/tasty')
+    end
 
-      expect(Fishistory.config.fish_root_path).to eq('~/fish/are/tasty')
+    it "sets default database settings" do
+      expect(subject.adapter).to  eq('sqlite3')
+      expect(subject.database).to eq('fishistory')
+      expect(subject.username).to eq('root')
+      expect(subject.password).to eq('password')
+    end
+
+    it "allows you to configure database settings" do
+      Fishistory.configure do |config|
+        config.adapter  = 'mysql2'
+        config.database = 'shrimp'
+        config.username = 'shark'
+        config.password = 'marlin'
+      end
+
+      expect(subject.adapter).to  eq('mysql2')
+      expect(subject.database).to eq('shrimp')
+      expect(subject.username).to eq('shark')
+      expect(subject.password).to eq('marlin')
     end
   end
 end
